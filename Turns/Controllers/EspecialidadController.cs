85e0dc587c74e82ed9ede47fdb3edbf21193c1c0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-21 09:52:28 +0300

    EntityFramework SQL connection

--- Turns/Controllers/EspecialidadController.cs
+++ Turns/Controllers/EspecialidadController.cs
@@ -1,17 +1,20 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Turns.Models;
 
 namespace Turns.Controllers
 {
     public class EspecialidadController : Controller
     {
-       public EspecialidadController()
+        private readonly TurnsContext context;
+       public EspecialidadController(TurnsContext context)
        {
-
+            this.context = context;
        }
 
         public IActionResult Index()
         {
-            return View();
+            return View(this.context.Speciality.ToList());
         }
     }
 }
