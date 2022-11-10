4b3353291936573732c1e13a8807dba1756df7cd
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-10 21:11:28 +0200

    MVC

---
+++ Turns/Controllers/SpecialityController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Turns.Controllers
+{
+    public class SpecialityController : Controller
+    {
+        public SpecialityController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
