63515912d5c853c1b8fd8753a1f770c41f2dce39
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-10 19:05:12 +0200

    MVC

---
+++ Turns/Models/Speciality.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Turns.Models
+{
+    public class Speciality
+    {
+        [Key]
+        public int SpecialtyId{get;set;}
+    }        
+}
