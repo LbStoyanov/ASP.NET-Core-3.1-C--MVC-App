550905ce3fcb97b49395efb6b0228629323798e1
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-18 12:38:05 +0200

    ASP.NET

--- Turns/Controllers/SpecialityController.cs
+++ Turns/Controllers/SpecialityController.cs
@@ -17,10 +17,38 @@ namespace Turns.Controllers
             return View(this._context.Specialities.ToList());
         }
 
-        public IActionResult Edit(int id)
+        public IActionResult Edit(int? id)
         {   
+
+            if (id == null)
+            {
+                return NotFound();             
+            }
+
             var speciality = this._context.Specialities.Find(id);
 
+            if (speciality == null)
+            {
+                return NotFound();
+            }
+
+            return View(speciality);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind("IdSpeciality,Description")] Speciality speciality)
+        {
+            if (id != speciality.SpecialtyId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                this._context.Update(speciality);
+                this._context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
             return View(speciality);
         }
     }
