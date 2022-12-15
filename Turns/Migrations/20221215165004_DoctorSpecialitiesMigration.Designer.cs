222bc145f6b2c20112611612a70354ab87454937
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-12-15 18:53:45 +0200

    EF Relations Added

--- Turns/Migrations/20221215165004_DoctorSpecialitiesMigration.Designer.cs
+++ Turns/Migrations/20221215165004_DoctorSpecialitiesMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Turns.Models;
 
@@ -11,9 +12,10 @@ using Turns.Models;
 namespace Turns.Migrations
 {
     [DbContext(typeof(TurnsContext))]
-    partial class TurnsContextModelSnapshot : ModelSnapshot
+    [Migration("20221215165004_DoctorSpecialitiesMigration")]
+    partial class DoctorSpecialitiesMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Turns/Migrations/20221215165004_DoctorSpecialitiesMigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Turns.Migrations
+{
+    public partial class DoctorSpecialitiesMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DoctorSpecialities",
+                columns: table => new
+                {
+                    DoctorId = table.Column<int>(type: "int", nullable: false),
+                    SpecialityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DoctorSpecialities", x => new { x.DoctorId, x.SpecialityId });
+                    table.ForeignKey(
+                        name: "FK_DoctorSpecialities_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "DoctorId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DoctorSpecialities_Specialities_SpecialityId",
+                        column: x => x.SpecialityId,
+                        principalTable: "Specialities",
+                        principalColumn: "SpecialityId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DoctorSpecialities_SpecialityId",
+                table: "DoctorSpecialities",
+                column: "SpecialityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DoctorSpecialities");
+        }
+    }
+}
