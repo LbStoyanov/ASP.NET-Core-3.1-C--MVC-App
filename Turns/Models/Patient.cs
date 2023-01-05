60dd6d9e56f0bfba121df0eb374b1a0e52c526e0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-01-05 19:16:34 +0200

    Added Data Anotation

--- Turns/Models/Patient.cs
+++ Turns/Models/Patient.cs
@@ -7,14 +7,24 @@ namespace Turns.Models
         [Key]
         public int PatientId {get; set;}
 
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; } = null!;
 
+        [Required]
+        [Display(Name = "Last Name")]
         public string  LastName { get; set; } = null!;
 
+        [Required]
         public string Direction { get; set; } = null!;
 
+        [Required]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; } = null!;
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = null!;
+
     }
 }
