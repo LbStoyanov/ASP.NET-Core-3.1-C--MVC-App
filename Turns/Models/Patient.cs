68e3ea4b014d47a8381a2f1d20dc0a09ed1bc9cb
LbStoyanov <lbstoyanov1@gmail.com>
2023-05-15 19:42:29 +0300

    Refactoring

--- Turns/Models/Patient.cs
+++ Turns/Models/Patient.cs
@@ -2,28 +2,28 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Turns.Models
 {
-    public class Patient
+    public partial class Patient
     {
         [Key]
         public int PatientId {get; set;}
 
-        [Required]
+        [Required(ErrorMessage ="You should enter your first name")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; } = null!;
 
-        [Required]
+        [Required(ErrorMessage ="You should enter your last name")]
         [Display(Name = "Last Name")]
         public string  LastName { get; set; } = null!;
 
-        [Required]
+        [Required(ErrorMessage ="You should enter your direction")]
         public string Direction { get; set; } = null!;
 
-        [Required]
+        [Required(ErrorMessage ="You should enter your phone number")]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; } = null!;
 
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage ="You should enter your email address")]
+        [EmailAddress(ErrorMessage = "This is not a valid email address")]
         public string Email { get; set; } = null!;
 
         public List<Turn> Turns {get; set;} = null!;
