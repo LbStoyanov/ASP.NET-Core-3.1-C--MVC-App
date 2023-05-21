efb4b460c78d8f17aaeffa02ed157a238e72b438
LbStoyanov <lbstoyanov1@gmail.com>
2023-05-21 14:28:50 +0300

    Add New Table Logins

--- Turns/Migrations/20230521112638_NewLoginTable.Designer.cs
+++ Turns/Migrations/20230521112638_NewLoginTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Turns.Models;
 
@@ -11,9 +12,10 @@ using Turns.Models;
 namespace Turns.Migrations
 {
     [DbContext(typeof(TurnsContext))]
-    partial class TurnsContextModelSnapshot : ModelSnapshot
+    [Migration("20230521112638_NewLoginTable")]
+    partial class NewLoginTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
