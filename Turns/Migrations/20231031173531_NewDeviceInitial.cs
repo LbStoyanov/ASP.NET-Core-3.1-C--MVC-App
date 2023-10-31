850eaa5a8ae16f4e632aa37a9361d4e51cf699c0
LbStoyanov <lbstoyanov1@gmail.com>
2023-10-31 19:40:22 +0200

    UpdateDeviceForDevelopment

---
+++ Turns/Migrations/20231031173531_NewDeviceInitial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Turns.Migrations
+{
+    public partial class NewDeviceInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
