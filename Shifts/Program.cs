14cfe0771ef0f1eb01d4df5298c04f2a0a626582
LbStoyanov <lbstoyanov1@gmail.com>
2024-09-30 06:22:42 +0300

    Refactoring

--- Shifts/Program.cs
+++ Shifts/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddSession(options =>
     options.IdleTimeout = TimeSpan.FromSeconds(300);
     options.Cookie.HttpOnly = true;
 });
-// Add services to the container.
+ 
 builder.Services.AddControllersWithViews(options =>
 {
     options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
@@ -19,11 +19,10 @@ builder.Services.AddDbContext<ShiftsContext>(options => options.UseSqlServer(bui
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    
     app.UseHsts();
 }
 
